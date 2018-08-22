174364131a4aff08b74223f50e7cc6ef390f3b37
Billy Brawner <billybrawner@gmail.com>
2018-08-21 22:25:00 -0500

    Add ACRA crash reporting

--- app/src/main/java/com/wbrawner/myallowance/AllowanceApplication.kt
+++ app/src/main/java/com/wbrawner/myallowance/AllowanceApplication.kt
@@ -1,12 +1,21 @@
 package com.wbrawner.myallowance
 
 import android.app.Application
-import android.arch.lifecycle.ViewModelProvider
 import android.arch.persistence.room.Room
-import android.arch.persistence.room.Transaction
+import android.content.Context
 import com.wbrawner.myallowance.data.TransactionDao
 import com.wbrawner.myallowance.data.TransactionsDatabase
+import org.acra.ACRA
+import org.acra.annotation.AcraCore
+import org.acra.annotation.AcraHttpSender
+import org.acra.data.StringFormat
+import org.acra.sender.HttpSender
 
+@AcraCore(reportFormat = StringFormat.JSON)
+@AcraHttpSender(uri = BuildConfig.ACRA_URL,
+        basicAuthLogin = BuildConfig.ACRA_USER,
+        basicAuthPassword = BuildConfig.ACRA_PASS,
+        httpMethod = HttpSender.Method.POST)
 class AllowanceApplication: Application() {
     lateinit var database: TransactionsDatabase
     private set
@@ -20,6 +29,11 @@ class AllowanceApplication: Application() {
         database = Room.databaseBuilder(applicationContext, TransactionsDatabase::class.java, "transactions")
                 .build()
         dao = database.dao()
+    }
 
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        ACRA.init(this)
     }
 }
