3980d2729b91b1336253306f66632a1e33d45e20
William Brawner <me@wbrawner.com>
2024-03-16 09:15:51 -0600

    Drop compose material
    Material3 is already present so legacy material is redundant

--- android/src/main/java/com/wbrawner/twigs/android/ui/recurringtransaction/RecurringTransactionsScreen.kt
+++ android/src/main/java/com/wbrawner/twigs/android/ui/recurringtransaction/RecurringTransactionsScreen.kt
@@ -13,8 +13,8 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
