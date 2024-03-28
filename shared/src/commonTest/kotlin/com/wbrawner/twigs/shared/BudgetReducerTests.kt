df65cb20242652230640ea858ae73963a7094cb4
William Brawner <me@wbrawner.com>
2024-03-27 21:28:52 -0600

    Fix test failures

--- shared/src/commonTest/kotlin/com/wbrawner/twigs/shared/BudgetReducerTests.kt
+++ shared/src/commonTest/kotlin/com/wbrawner/twigs/shared/BudgetReducerTests.kt
@@ -1,44 +1,45 @@
 package com.wbrawner.twigs.shared
 
+import com.russhwolf.settings.Settings
 import com.wbrawner.twigs.shared.budget.BudgetAction
 import com.wbrawner.twigs.shared.budget.BudgetReducer
-import com.wbrawner.twigs.shared.budget.BudgetRepository
 import com.wbrawner.twigs.shared.user.Permission
 import com.wbrawner.twigs.shared.user.UserPermission
-import kotlinx.datetime.toInstant
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.test.setMain
 import kotlin.test.BeforeTest
 import kotlin.test.DefaultAsserter.assertEquals
-import kotlin.test.DefaultAsserter.assertTrue
 import kotlin.test.Test
 import kotlin.test.assertFalse
 import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 class BudgetReducerTests {
     lateinit var reducer: BudgetReducer
     lateinit var dispatchedActions: MutableList<Action>
     lateinit var budgetRepository: FakeBudgetRepository
+    lateinit var settings: Settings
 
+    @OptIn(ExperimentalCoroutinesApi::class, DelicateCoroutinesApi::class)
     @BeforeTest
     fun setup() {
+        Dispatchers.setMain(newSingleThreadContext("main"))
         dispatchedActions = mutableListOf()
         budgetRepository = FakeBudgetRepository()
-        reducer = BudgetReducer(budgetRepository)
+        settings = FakeSettings()
+        reducer = BudgetReducer(budgetRepository, settings)
         reducer.dispatch = { dispatchedActions.add(it) }
     }
 
     @Test
     fun goBackWhileEditingTest() {
         val state = State(editingBudget = true, selectedBudget = "test")
-        val newState = reducer.reduce(Action.Back, state)
+        val newState = reducer.reduce(Action.Back) { state }
         assertFalse(newState.editingBudget)
         assertEquals("selectedBudget should still be set", "test", newState.selectedBudget)
-    }
-
-    @Test
-    fun goBackWhileViewingTest() {
-        val state = State(selectedBudget = "test")
-        val newState = reducer.reduce(Action.Back, state)
-        assertNull(newState.selectedBudget)
     }
 
     @Test
@@ -46,9 +47,8 @@ class BudgetReducerTests {
         val state = State()
         val users = listOf(UserPermission("user", Permission.OWNER))
         assertFalse(state.loading)
-        val newState = reducer.reduce(BudgetAction.CreateBudget("test", "description", users), state)
-        assertTrue(state.loading)
-
+        val newState = reducer.reduce(BudgetAction.CreateBudget("test", "description", users)) { state }
+        assertTrue(newState.loading)
         assertNull(newState.selectedBudget)
     }
 }
